d922b4098e1311314c5f4489a80cada2b485a29a
MarcReyes99 <146344103+MarcReyes99@users.noreply.github.com>
2024-02-09 11:11:56 +0100

    ferf

--- Assets/Scripts/Bala.cs
+++ Assets/Scripts/Bala.cs
@@ -6,8 +6,6 @@ public class Bala : MonoBehaviour
 {
     Rigidbody2D rb;
     public float bullet_speed;
-    public bool target_enemies = true;
-    public bool target_building = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,14 +16,14 @@ public class Bala : MonoBehaviour
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (target_enemies && collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.tag == "Enemy")
         {
             Destroy(collision.gameObject);
             Destroy(gameObject);
-            MovimientoEnemigos velocidadEnemy = collision.gameObject.GetComponent<MovimientoEnemigos>();
-            velocidadEnemy.velocidadHorizontal += 0.25f;
+            MovimientoEnemigos velocidadEnemy = FindObjectOfType<MovimientoEnemigos>();
+            velocidadEnemy.velocidadHorizontal += 0.1f;
         }
-        if (target_building && collision.gameObject.tag == "Building")
+        if (collision.gameObject.tag == "Building")
         {
             collision.gameObject.transform.localScale += new Vector3(-2, 0, 0);
             Destroy(gameObject);
